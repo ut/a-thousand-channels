897ec46709e213be1419abcf5ec7b81dcd284b21
ut <ut@users.noreply.github.com>
2022-02-01 19:27:55 +0000

    deploy: 2313c03421ffac3824ac2139b28bf9f5e57ceb68

---
+++ _nuxt/static/1643743674/en/privacy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fa-thousand-channels\u002F_nuxt\u002Fstatic\u002F1643743674",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fprivacy",config:{_app:{basePath:"\u002Fa-thousand-channels\u002F",assetsPath:"\u002Fa-thousand-channels\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4f1fd557"}}}}(null));
